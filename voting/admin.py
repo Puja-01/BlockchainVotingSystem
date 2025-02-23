1ced68405fbb8d6c25fc888341c0e97accd59b7b
Puja-01 <pr045739@gmail.com>
2025-02-23 14:36:32 +0530

    Registered all models in Django admin

--- voting/admin.py
+++ voting/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import Election, Candidate, Vote, Voter
+
+admin.site.register(Voter)
+admin.site.register(Election)
+admin.site.register(Candidate)
+admin.site.register(Vote)
+
+
 # Register your models here.
